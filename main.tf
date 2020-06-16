d6841b877b8f69d0dd681f3296cd0872acdf68a0
Grant Orchard <go@hashicorp.com>
2020-06-16 13:23:53 +1000

    added nsx self service workspace

--- main.tf
+++ main.tf
@@ -155,3 +155,14 @@ module "terraform-nsx-core" {
   create_repo = false
   oauth_token_id = var.oauth_token_id
 }
+
+module "terraform-nsx-selfserve" {
+  source = "app.terraform.io/grantorchard/workspace/tfe"
+  providers = {
+    github = github.personal
+  }
+  repository_name = "terraform-nsx-selfserve"
+  create_repo = true
+  oauth_token_id = var.oauth_token_id
+  env_var = var.aws_env_var
+}
