709522fe58222208a3151514d01a3e65b92e0f4d
Grant Orchard <go@hashicorp.com>
2020-07-10 15:18:26 +1000

    Added RMS repo

--- main.tf
+++ main.tf
@@ -176,3 +176,14 @@ module "terraform-aws-venafi" {
   oauth_token_id = var.oauth_token_id
   env_var = var.aws_env_var
 }
+
+module "terraform-aws-rms" {
+  source = "app.terraform.io/grantorchard/workspace/tfe"
+  providers = {
+    github = github.personal
+  }
+  repository_name = "terraform-aws-rms"
+  create_repo = true
+  oauth_token_id = var.oauth_token_id
+  env_var = var.aws_env_var
+}
